3a02434b74d1e0bc70b10c3438e0d36bc952acfb
Preeti Chib <preeti.chib@tothenew.com>
2025-02-03 22:07:38 +0530

    Inital commit

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Intro to java Assignment 1
+### Submited by Preeti Chib 
+### JVM (8228)
+### Dated 3 Feb 2025
---
+++ src/Question2.java
@@ -0,0 +1,10 @@
+/* Q2. Write a program to read user input until the user writes
+    XDONE and then show the entered text by the user on command line.*/
+
+public class Question2 {
+    public static void main(String[] args){
+
+
+        
+    }
+}
